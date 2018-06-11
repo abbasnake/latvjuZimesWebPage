0c7e6b939b01e090bae2eedd19b7c045b5eb81b1
abbasnake <mr.rihards.ozols@gmail.com>
2018-06-11 21:46:55 +0300

    stylized sliders

--- client/src/components/AppControlsSlider.vue
+++ client/src/components/AppControlsSlider.vue
@@ -12,6 +12,7 @@
       :value="sliderValue"
       :id="sliderName"
       @input="onChange"
+      :style="bgColor"
     >
   </div>
 </template>
@@ -29,12 +30,35 @@ export default {
       default: () => ({min: 0, max: 255})
     }
   },
+  computed: {
+    bgColor () {
+      return this.returnBgColor()
+    }
+  },
+  mounted () {
+    console.log(this.sliderName)
+  },
   methods: {
     onChange (e) {
       this.$store.dispatch('changeSlider', {
         name: this.sliderName,
         value: e.target.value
       })
+    },
+    returnBgColor () {
+      switch (this.sliderName) {
+        case 'r':
+        case 'bgR':
+          return {'background-color': `rgb(${this.sliderValue}, 0, 0)`}
+        case 'g':
+        case 'bgG':
+          return {'background-color': `rgb(0, ${this.sliderValue}, 0)`}
+        case 'b':
+        case 'bgB':
+          return {'background-color': `rgb(0, 0, ${this.sliderValue})`}
+        default:
+          return {'background-color': '#740D09'}
+      }
     }
   }
 }
@@ -47,15 +71,41 @@ export default {
     "input label";
   grid-template-columns: 1fr 1fr;
   padding: 5px;
-
   &__input {
     grid-area: input;
+    cursor: pointer;
     margin: 0 auto;
     width: 90%;
+    -webkit-appearance: none;
+    height: 3px;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+    align-self: center;
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      border: 1px solid #740D09;
+      background: white;
+      cursor: pointer;
+    }
+    &::-moz-range-thumb {
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+    &:hover {
+      opacity: 1.0;
+    }
   }
   &__label {
     grid-area: label;
     text-align: center;
+    padding-bottom: 4px
   }
 }
 
